82106e5b15826bad099a7fd91a0a91e27548a9e7
Tim Qin <yqin@ucsb.edu>
2023-05-16 15:01:36 -0700

    backend added for energy drink

---
+++ src/main/java/edu/ucsb/cs156/example/entities/EnergyDrink.java
@@ -0,0 +1,34 @@
+package edu.ucsb.cs156.example.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "energydrink")
+public class EnergyDrink {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String flavor;
+
+    @NotNull
+    @Column(unique=true)
+    private String calories;
+}
